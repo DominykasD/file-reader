a2d2bf5aeaf5314cc047bb28778d8dd92a7f6f1c
DominykasD <dominykasdrevinskas@gmail.com>
2023-07-19 19:41:04 +0300

    Fixed CsvFormat.php and added data format argument in cmd

--- formats/CsvFormat.php
+++ formats/CsvFormat.php
@@ -15,11 +15,21 @@ class CsvFormat
     public function parse(string $filename): array
     {
         $data = [];
-        $file = fopen($filename, 'r');
-        while (($row = fgetcsv($file)) !== false) {
-            $data[] = $row;
+        $header = null;
+        $delimiter = ',';
+        $enclosure = '"';
+        $escape = '\\';
+
+        if (($handle = fopen($filename, 'r')) !== false) {
+            while (($row = fgetcsv($handle, 1000, $delimiter, $enclosure, $escape)) !== false) {
+                if (!$header) {
+                    $header = $row;
+                } else {
+                    $data[] = array_combine($header, $row);
+                }
+            }
+            fclose($handle);
         }
-        fclose($file);
 
         return $data;
     }
--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 require_once __DIR__ . '/helpers/FileLoader.php';
 
-$file = __DIR__ . '/data/data.json';
+// Check if the user provided the data format as a command-line argument
+if ($argc < 2) {
+    echo "Usage: php index.php [csv|xml|json]\n";
+    exit(1);
+}
 
-$fileLoader = new \App\Helpers\FileLoader();
+// Get the data format from the command-line argument
+$dataFormat = strtolower($argv[1]);
+
+// Validate the data format
+$allowedFormats = ['csv', 'xml', 'json'];
+if (!in_array($dataFormat, $allowedFormats)) {
+    echo "Invalid data format. Available formats: csv, xml, json\n";
+    exit(1);
+}
+
+$file = __DIR__ . '/data//' . 'data.' . $dataFormat;
+
+$fileLoader = new App\Helpers\FileLoader();
 $data = $fileLoader->loadFile($file);
 
 print_r($data);
